72d8630c7fda8933af10f5c57afb677bb3e75c07
影凌 <k6.developer@gmail.com>
2024-07-04 16:35:43 +0800

    feat: Add put posts.

---
+++ API_practice/put.py
@@ -0,0 +1,13 @@
+import requests
+
+api_url = "https://jsonplaceholder.typicode.com/posts/1"
+
+data = {
+    "userId": 1,
+    "id": 1,
+    "title": "Hambuger",
+    "body": "good"
+}
+
+response = requests.put(api_url, json=data)
+print(response.json())
