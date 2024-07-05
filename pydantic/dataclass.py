af8dfdeb20175fe6b809aa4bdf221dbbc78c2b19
影凌 <k6.developer@gmail.com>
2024-07-05 15:19:48 +0800

    refactor: traditional -> dataclass

---
+++ pydantic/dataclass.py
@@ -0,0 +1,19 @@
+from dataclasses import dataclass
+import logging
+
+# 設定 logging 的 format
+logging.basicConfig(
+    format="[%(asctime)s][%(name)-5s][%(levelname)-5s] %(message)s (%(filename)s:%(lineno)d)",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+member_logger = logging.getLogger('member')
+member_logger.setLevel(logging.INFO)
+
+@dataclass
+class Member:
+    name: str
+    phone: str
+    email: str
+
+member1 = Member('abbc','0900123456','abbc@gmail.com')
+member_logger.info(member1)
