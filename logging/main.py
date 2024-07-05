4a361723999eaad28813b929022b7d6fe3134b2a
影凌 <k6.developer@gmail.com>
2024-07-05 11:12:54 +0800

    feat: Add logger create practice.

--- logging/main.py
+++ logging/main.py
@@ -6,6 +6,10 @@ logging.basicConfig(level=logging.INFO)
 logging.info('Hi!')
 # 比較完整的方式
 root_logger = logging.getLogger()
-root_logger.setLevel(logging.INFO)
-root_logger.info('Hi!')
+root_logger.setLevel(logging.WARNING)
+root_logger.warning('oh no!')
 
+# Create a logger
+main_logger = logging.getLogger('main')     # 父 Logger
+sub_logger = logging.getLogger('main.sub')  # 子 Logger
+sub_logger = main_logger.getChild('sub')    # 子 Logger（另一種寫法）
