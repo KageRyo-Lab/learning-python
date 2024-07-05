6ca8ec6e5ac95798737f12169f6724a553c068d1
影凌 <k6.developer@gmail.com>
2024-07-05 16:51:48 +0800

    feat: Add post init.

--- pydantic/dataclass.py
+++ pydantic/dataclass.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+import datetime
 import logging
 
 # 設定 logging 的 format
@@ -14,8 +15,13 @@ class Member:
     name: str
     phone: str
     email: str
+    record_time: datetime.datetime = field(init=False, default_factory=datetime.datetime.now)
+    def __post_init__(self):
+        self.information = f'{self.name} {self.phone} {self.email}'
 
-member1 = Member('abbc','0900123456','abbc@gmail.com')
-member2 = Member('people','0912345678','peo@yahoo.com.tw')
+member1 = Member('steve','0900123456','uwu@gmail.com')
+member2 = Member('people','0912345678','people@yahoo.com.tw')
 member_logger.info(member1)
+member_logger.info(member1.information)
 member_logger.info(member2)
+member_logger.info(member2.information)
